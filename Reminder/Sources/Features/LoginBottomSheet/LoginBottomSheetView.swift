c2aff4c724b03df8989c0945a4fff8e7ea3277ac
Alan Pitanga <alansantosbahia@gmail.com>
2025-03-09 20:39:26 -0300

    adjustment

--- Reminder/Sources/Features/LoginBottomSheet/LoginBottomSheetView.swift
+++ Reminder/Sources/Features/LoginBottomSheet/LoginBottomSheetView.swift
@@ -18,15 +18,6 @@ class LoginBottomSheetView: UIView {
         return view
     }()
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.text = "login.lable.title".localized
