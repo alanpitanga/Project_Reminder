56d5d7d551752294b23953a27c4506ac00133ede
Alan Pitanga <alansantosbahia@gmail.com>
2025-03-11 15:55:29 -0300

    installing pods and implementing firebase

--- Reminder/Sources/Features/LoginBottomSheet/LoginBottomSheetViewDelegate.swift
+++ Reminder/Sources/Features/LoginBottomSheet/LoginBottomSheetViewDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import UIKit
+import Firebase
 
 protocol LoginBottomSheetViewDelegate: AnyObject {
     
--- Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
+++ Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
@@ -6,9 +6,17 @@
 //
 
 import Foundation
+import Firebase
 
 class LoginBottomSheetViewModel {
     func doAuth(userNameLogin: String, password: String) {
-        
+        print(userNameLogin)
+        Auth.auth().signIn(withEmail: userNameLogin, password: password) {[ weak self ] authResult, error in
+            if let error = error {
+                print("Falha na autenticação\(error )")
+            } else {
+                print(authResult, "Autenticado com Sucesso")
+            }
+        }
     }
 }
