d9e14d74b206c39905e7bcf5e8f2ed920f36b24f
Alan Pitanga <alansantosbahia@gmail.com>
2025-03-30 14:25:54 -0300

    creation of the "Scenes" folder to organize the project and basic structure of the Home screen

---
+++ Reminder/Sources/Features/Scenes/Home/HomeFlowDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  HomeFlowDelegate.swift
+//  Reminder
+//
+//  Created by Alan Pitanga on 30/03/25.
+//
+
+import Foundation
---
+++ Reminder/Sources/Features/Scenes/Home/HomeView.swift
@@ -0,0 +1,8 @@
+//
+//  HomeView.swift
+//  Reminder
+//
+//  Created by Alan Pitanga on 30/03/25.
+//
+
+import Foundation
